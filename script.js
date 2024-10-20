baa31b9ceff0fd244862c1f263cdc8bb8225b54b
JoshSebben <sebbenjosh@gmail.com>
2024-10-20 18:58:51 -0400

    create 16x16 grid

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 function grid(){
     for (let i = 0; i < 16; i++) {
+        const divContainer = document.querySelector("#container")
+
+        const divHolder = document.createElement("div");
+        divHolder.setAttribute("id", "holder"+i)
+
+        divContainer.appendChild(divHolder)
+
         for (let index = 0; index < 16; index++){
-            const container = document.querySelector("#container");
+            const container = document.querySelector("#holder"+i);
 
             const divs = document.createElement("div");
-            divs.classList.add("grid");
-            divs.style.color = "blue";
-            divs.style.borderColor = "black"
+            divs.classList.add("grid"+i);
 
             container.appendChild(divs)
-    
         }
         
     }
