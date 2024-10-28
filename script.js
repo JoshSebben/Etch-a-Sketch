b1724dc3598ccbae7fa8040ce7357012c60aeb5e
JoshSebben <sebbenjosh@gmail.com>
2024-10-28 15:37:11 -0400

    added function for users to choose their grid size

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
-/*const button = document.querySelector("#gridSelector")
-
-button.onclick = gridSizeChooser();
-
-function gridSizeChooser(){
-    let number = prompt("Choose size of grid (1-100)")
-    parseInt(number)
-    return number
-}*/
-
 function grid(){
-    for (let i = 0; i < gridSizeChooser(); i++) {
+    for (let i = 0; i < 16; i++) {
         const divContainer = document.querySelector("#container")
 
         const divHolder = document.createElement("div");
@@ -17,7 +7,7 @@ function grid(){
 
         divContainer.appendChild(divHolder)
 
-        for (let index = 0; index < gridSizeChooser(); index++){
+        for (let index = 0; index < 16; index++){
             const container = document.querySelector("#holder"+i);
 
             const divs = document.createElement("div");
@@ -32,5 +22,44 @@ function grid(){
 
 }
 
-//gridSizeChooser();
+function gridSizeChooser(){
+    const divContainer = document.querySelector("#container")
+
+    //Removes previous grid
+    while (divContainer.firstChild) {
+        divContainer.removeChild(divContainer.lastChild);
+      }
+
+    let size = parseInt(prompt("Choose size 1-100"))
+
+    //Replaces new grid with new one
+    if (size <= 100){
+        for (let i = 0; i < size; i++) {
+            const divContainer = document.querySelector("#container")
+
+            const divHolder = document.createElement("div");
+            divHolder.setAttribute("id", "holder"+i)
+
+            divContainer.appendChild(divHolder)
+
+            for (let index = 0; index < size; index++){
+                const container = document.querySelector("#holder"+i);
+
+                const divs = document.createElement("div");
+                divs.classList.add("grid"+i);
+                divs.onmouseover = () => divs.style.backgroundColor = "black"
+                divs.onmouseout = () => divs.style.backgroundColor = "white"
+
+                container.appendChild(divs)
+            }
+            
+        }
+    }
+    else alert("Number is too big or small")
+
+
+
+}
+
+
 grid();
